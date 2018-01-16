e4401315a0c4e6a0087eccd239603dca71503395
DnCLab <35354684+DnCLab@users.noreply.github.com>
2018-01-16 17:59:01 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
+
 var app = express();
 
 app.use(express.static('public'));
 
+app.get("/public", function(req, res) {
+  res.send("Test");
+
+});
+
 app.listen(3000, function() {
-  console.log('Connected 3000 port');
+  console.log('Server App started on 3000 port');
 });
